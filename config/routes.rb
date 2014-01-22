b108c998096c95fdba005ece091c5b904a617fd7
wesley-stafford <staffordw1@mymail.nku.edu>
2014-01-22 20:20:16 +0000

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NkuRails::Application.routes.draw do
+  get "welcome/idex"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
