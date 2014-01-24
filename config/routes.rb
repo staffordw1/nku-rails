2f4fe6388a1b02bb6d6796aaf0fc8d5f071c96e7
wesley-stafford <staffordw1@mymail.nku.edu>
2014-01-24 01:08:05 +0000

    little past like 6.3, i think

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ NkuRails::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   # You can have the root of your site routed with "root"
   root to: "welcome#index"
